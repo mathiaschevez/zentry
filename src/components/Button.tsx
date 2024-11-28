750ec8b6fa1487ef980ee29f092ceb284a05267d
Mathias Chevez <davidchevez90@gmail.com>
2024-11-27 19:17:57 -0700

    add audio button and build out navbar

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,19 @@
+type ButtonProps = { 
+  id: string,
+  title: string,
+  leftIcon?: React.ReactNode,
+  rightIcon?: React.ReactNode,
+  containerClass: string
+}
 
-export default function Button({ id, title, leftIcon, containerClass }: { id: string, title: string, leftIcon: React.ReactNode, containerClass: string }) {
+export default function Button({ id, title, leftIcon, rightIcon, containerClass }: ButtonProps) {
   return (
     <div id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
       {leftIcon}
       <span className="relative inline-flex overlow-hidden font-general text-xs uppercase">
         <div>{title}</div>
       </span>
-      
+      {rightIcon}
     </div>
   )
 }
